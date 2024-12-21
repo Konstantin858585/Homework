8af58c6463dd2ba9b0abd03472594cf7c677fc3f
Konstantin858585 <petrovetckonstantin@gmail.com>
2024-12-21 21:22:07 +0500

    21.12.24

---
+++ function call counter.py
@@ -0,0 +1,36 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+
+def string_info(string):
+    string = input('Введите строку: ', )
+    print(len(string))
+    print(string.lower())
+    print(string.upper())
+    count_calls()
+
+
+
+def  is_contains(string, list_to_search):
+    string = input('Введите искомую строку: ', )
+    string.lower()
+    list_ = input('Введите список из которого нужно выделить искомую строку через запятую :', )
+    list_.lower()
+    list_to_search = list_.split(',')
+    print(list_to_search)
+    if string in list_to_search:
+        print(True)
+    else:
+        print(False)
+    count_calls()
+
+
+string_info('')
+string_info('')
+is_contains('',[])
+
+print(calls)
